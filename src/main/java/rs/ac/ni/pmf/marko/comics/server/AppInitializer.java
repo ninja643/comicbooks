caf5d1147d2a4f67596d1c744072112daba4f21b
Marko Milosevic <ninja643@gmail.com>
2018-04-04 15:41:05 +0200

    Add AppInitializer

---
+++ src/main/java/rs/ac/ni/pmf/marko/comics/server/AppInitializer.java
@@ -0,0 +1,27 @@
+package rs.ac.ni.pmf.marko.comics.server;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class AppInitializer implements WebApplicationInitializer
+{
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException
+	{
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		// context.register(ApplicationConfiguration.class);
+
+		ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher",
+				new DispatcherServlet(context));
+		registration.setLoadOnStartup(1);
+		registration.addMapping("/rest/*");
+
+	}
+
+}
