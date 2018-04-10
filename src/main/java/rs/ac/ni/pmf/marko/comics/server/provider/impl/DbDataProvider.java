0e076e49ddc4b43466d0a7b6abf6aea5bda55a1a
Marko Milosevic <ninja643@gmail.com>
2018-04-10 14:34:47 +0200

    Add empty DbDataProvider

---
+++ src/main/java/rs/ac/ni/pmf/marko/comics/server/provider/impl/DbDataProvider.java
@@ -0,0 +1,17 @@
+package rs.ac.ni.pmf.marko.comics.server.provider.impl;
+
+import java.util.List;
+
+import rs.ac.ni.pmf.marko.comics.server.datamodel.ComicBook;
+import rs.ac.ni.pmf.marko.comics.server.provider.DataProvider;
+
+public class DbDataProvider implements DataProvider
+{
+
+	@Override
+	public List<ComicBook> getAllComicBooks()
+	{
+		return null;
+	}
+
+}
