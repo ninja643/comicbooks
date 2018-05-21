1ca0323e4bfe81efb92654f05fc27ffdd4a64934
Marko Milosevic <markom@niri.rs>
2018-05-21 22:49:45 +0200

    Remove /single service as it is not needed

--- src/main/java/rs/ac/ni/pmf/marko/comics/server/rest/ComicBooksRestService.java
+++ src/main/java/rs/ac/ni/pmf/marko/comics/server/rest/ComicBooksRestService.java
@@ -23,10 +23,4 @@ public class ComicBooksRestService
 	{
 		return dataProvider.getAllComicBooks();
 	}
-	
-	@RequestMapping(value = "/single", method = RequestMethod.GET, produces = MediaType.APPLICATION_XML_VALUE)
-	public ComicBook getSingleComicBook()
-	{
-		return dataProvider.getAllComicBooks().get(0);
-	}	
 }
