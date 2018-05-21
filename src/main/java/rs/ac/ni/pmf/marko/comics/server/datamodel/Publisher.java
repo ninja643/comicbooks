0b0576cd6448c2c07164e500c7461cce4a9c693d
Marko Milosevic <markom@niri.rs>
2018-05-21 23:31:27 +0200

    Add comic books to publisher

--- src/main/java/rs/ac/ni/pmf/marko/comics/server/datamodel/Publisher.java
+++ src/main/java/rs/ac/ni/pmf/marko/comics/server/datamodel/Publisher.java
@@ -1,10 +1,13 @@
 package rs.ac.ni.pmf.marko.comics.server.datamodel;
 
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Version;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -29,4 +32,7 @@ public class Publisher
 
 	@Column(name = "name", length = 200, nullable = false)
 	private String name;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "publisher")
+	private Set<ComicBook> comicBooks;
 }
