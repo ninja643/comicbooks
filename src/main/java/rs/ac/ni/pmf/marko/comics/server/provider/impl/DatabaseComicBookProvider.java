5c90c7ea837c27063664a3f5a7b352ebbe0dfb07
Marko Milosevic <markom@niri.rs>
2018-05-21 23:32:40 +0200

    Implement addComicBook

--- src/main/java/rs/ac/ni/pmf/marko/comics/server/provider/impl/DatabaseComicBookProvider.java
+++ src/main/java/rs/ac/ni/pmf/marko/comics/server/provider/impl/DatabaseComicBookProvider.java
@@ -1,9 +1,7 @@
 package rs.ac.ni.pmf.marko.comics.server.provider.impl;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import rs.ac.ni.pmf.marko.comics.server.datamodel.ComicBook;
 import rs.ac.ni.pmf.marko.comics.server.jpa.ComicBooksRepository;
 import rs.ac.ni.pmf.marko.comics.server.provider.ComicBookProvider;
@@ -22,8 +20,7 @@ public class DatabaseComicBookProvider implements ComicBookProvider
 	@Override
 	public ComicBook addComicBook(ComicBook comicBook)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return _comicBooksRepository.save(comicBook);
 	}
 
 }
