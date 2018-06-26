c633fcdc261345998fad0a82e87ede56aac774eb
Marko Milosevic <ninja643@gmail.com>
2018-06-26 15:51:51 +0200

    Use the right PublisherDTO

--- src/main/java/rs/ac/ni/pmf/marko/comics/server/datamodel/converter/PublisherConverter.java
+++ src/main/java/rs/ac/ni/pmf/marko/comics/server/datamodel/converter/PublisherConverter.java
@@ -2,7 +2,7 @@ package rs.ac.ni.pmf.marko.comics.server.datamodel.converter;
 
 import org.springframework.stereotype.Component;
 
-import rs.ac.ni.pmf.marko.comics.server.api.PublisherDTO;
+import rs.ac.ni.pmf.marko.comics.server.datamodel.api.PublisherDTO;
 import rs.ac.ni.pmf.marko.comics.server.datamodel.entity.PublisherEntity;
 
 @Component
