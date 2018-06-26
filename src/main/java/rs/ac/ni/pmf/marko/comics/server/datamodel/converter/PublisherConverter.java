a59e49bc444adff8c8fecfcb08f62503e34539a2
Marko Milosevic <ninja643@gmail.com>
2018-06-26 15:50:13 +0200

    Add initial version of PublisherConverter

---
+++ src/main/java/rs/ac/ni/pmf/marko/comics/server/datamodel/converter/PublisherConverter.java
@@ -0,0 +1,15 @@
+package rs.ac.ni.pmf.marko.comics.server.datamodel.converter;
+
+import org.springframework.stereotype.Component;
+
+import rs.ac.ni.pmf.marko.comics.server.api.PublisherDTO;
+import rs.ac.ni.pmf.marko.comics.server.datamodel.entity.PublisherEntity;
+
+@Component
+public class PublisherConverter {
+
+	public PublisherDTO dtoFromEntity(PublisherEntity entity)
+	{
+		return new PublisherDTO(entity.getId(), entity.getName());
+	}
+}
