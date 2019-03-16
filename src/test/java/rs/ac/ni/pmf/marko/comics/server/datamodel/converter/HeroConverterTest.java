c068efcffddb8702301ea882f372377390085ee0
Marko Milosevic <ninja643@gmail.com>
2019-03-16 16:55:51 +0100

    Add tests

---
+++ src/test/java/rs/ac/ni/pmf/marko/comics/server/datamodel/converter/HeroConverterTest.java
@@ -0,0 +1,29 @@
+package rs.ac.ni.pmf.marko.comics.server.datamodel.converter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import mockit.Tested;
+import org.junit.Test;
+import rs.ac.ni.pmf.marko.comics.server.datamodel.api.HeroDTO;
+import rs.ac.ni.pmf.marko.comics.server.datamodel.entity.HeroEntity;
+
+import java.util.Collections;
+
+public class HeroConverterTest
+{
+	@Tested
+	private HeroConverter _converter;
+
+	@Test
+	public void shouldConvertHeroEntity()
+	{
+		final String name = "Zagor";
+
+		final HeroEntity heroEntity = HeroEntity.builder().id(1L).name(name).comicBooks(Collections.emptyList()).build();
+		final HeroDTO expectedHeroDto = HeroDTO.builder().id(1L).name(name).build();
+
+		final HeroDTO actualHeroDto = _converter.dtoFromEntity(heroEntity);
+
+		assertThat(actualHeroDto).isEqualTo(expectedHeroDto);
+	}
+}
